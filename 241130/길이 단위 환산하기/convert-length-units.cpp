349ed28f616b341bc4e6935ec7c67cbc76f4b28a
HyeonSeung Oh <77887324+raymond1203@users.noreply.github.com>
2024-11-30 18:19:43 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double ft = 30.48;
+    double n; cin >> n;
+    cout << fixed; cout.precision(1);
+    cout << n*ft;
+    return 0;
+}
