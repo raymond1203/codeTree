888d599edca68c4f16d6b5d043233495fc65b0a2
raymond1203 <77887324+raymond1203@users.noreply.github.com>
2024-11-30 17:09:23 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int sum = a + b + c;
+    a = sum; b = sum; c = sum;
+    cout << a << " " << b << " " << c;
+    return 0;
+}
