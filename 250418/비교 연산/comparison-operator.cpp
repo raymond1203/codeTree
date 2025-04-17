c3da8cffbc7db919971ef4accc827309fc610042
HyeonSeung Oh <77887324+raymond1203@users.noreply.github.com>
2025-04-18 01:03:44 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250418/비교 연산/comparison-operator.cpp
+++ 250418/비교 연산/comparison-operator.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 void decision(int a, int b, string s) {
-    if (a s b) {
-        cout << 1 << endl;
+    if (s == ">=") {
+        cout << (a >= b) << endl;
     }
-    else{
-        cout << 0 << endl;
+    else if (s == ">") {
+        cout << (a > b) << endl;
+    }
+    else if (s == "==") {
+        cout << (a == b) << endl;
+    }
+    else if (s == "!=") {
+        cout << (a != b) << endl;
+    }
+    else {
+        cout << "Invalid operator" << endl;
     }
 }
 
 int main() {
     int a, b; cin >> a >> b;
-    decision(a, b, >=);
-    decision(a, b, >=);
-    decision(a, b, >=);
+    decision(a, b, ">=");
+    decision(a, b, ">");
+    decision(b, a, ">=");
+    decision(b, a, ">");
+    decision(a, b, "==");
+    decision(a, b, "!=");
     return 0;
 }
