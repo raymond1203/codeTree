98e9ae453080534426627c3ed95d4b6ff9c08604
HyeonSeung Oh <77887324+raymond1203@users.noreply.github.com>
2025-04-18 00:42:12 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250418/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    float a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed << setprecision(3);
+    cout << a << endl << b << endl << c;
+    return 0;
+}
