2d2cefe1e64950b95df10e810e8dbdeb4830aeb6
HyeonSeung Oh <77887324+raymond1203@users.noreply.github.com>
2025-04-24 14:48:16 +0900

    Title: 100점 도달하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250424/100점 도달하기/reach-100-point.cpp
+++ 250424/100점 도달하기/reach-100-point.cpp
@@ -6,8 +6,8 @@ int main() {
     for(int i = n; i <= 100; i++){
         if(i>=90) cout << "A ";
         else if(i>=80) cout << "B ";
-        else if(i>=80) cout << "C ";
-        else if(i>=80) cout << "D ";
+        else if(i>=70) cout << "C ";
+        else if(i>=60) cout << "D ";
         else cout << "F ";
     }
     return 0;
